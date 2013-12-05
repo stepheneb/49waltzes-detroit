87aff66923d9ef530152d6cbbd1d348c167fc99b
Stephen Bannasch <stephen.bannasch@gmail.com>
2013-12-05 00:34:04 -0500

    support IE11 full-screen API
    see:  http://msdn.microsoft.com/en-us/library/ie/dn265028(v=vs.85).aspx

--- javascript/fullscreen.js
+++ javascript/fullscreen.js
@@ -3,6 +3,7 @@ function addFullScreenChangeListener(fullScreenChangeCallback) {
   document.addEventListener("fullscreenchange", fullScreenChangeCallback, false);
   document.addEventListener("mozfullscreenchange", fullScreenChangeCallback, false);
   document.addEventListener("webkitfullscreenchange", fullScreenChangeCallback, false);
+  document.addEventListener("MSFullscreenChange", fullScreenChangeCallback, false);  
 }
 
 function setupFullScreenSupport() {
@@ -10,19 +11,20 @@ function setupFullScreenSupport() {
        document.body.requestFullScreen ||
        document.body.webkitRequestFullScreen ||
        document.body.mozRequestFullScreen ||
-       document.body.msRequestFullScreen;
+       document.body.msRequestFullscreen;
 
   document.cancelFullscreenMethod =
        document.cancelFullScreen ||
        document.webkitCancelFullScreen ||
        document.mozCancelFullScreen ||
-       document.msCancelFullScreen;
+       document.msExitFullscreen;
 }
 
 function isFullscreen() {
   if (document.fullscreenElement||
       document.webkitFullscreenElement ||
-      document.mozFullScreenElement) {
+      document.mozFullScreenElement  ||
+      document.msFullscreenElement) {
     return true;
   }
 }
